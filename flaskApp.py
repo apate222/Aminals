06bb86b91f38e68a78eb47dea5473c628b3392d8
cwill399 <79541301+cwill399@users.noreply.github.com>
2021-11-12 16:58:45 -0500

    Added virtual environment, changes to html and py files.

--- flaskApp.py
+++ flaskApp.py
@@ -6,17 +6,14 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
-#from database import db
-#from models import Note as Note
-#from models import User as User
+from database import db
+from models import Question as Question
+from models import User as User
 
 app = Flask(__name__)     # create an app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-a_user = {'name': 'Casey', 'email':'cwill399@uncc.edu'}
-questions = {1: {'title': 'First Note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second Note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third Note', 'text': 'This is my third note', 'date': '10-3-2020'}
-             }
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -37,7 +34,20 @@ def get_question(q_id):
 
 @app.route('/questions/new', methods=['GET', 'POST'])
 def new_question():
-    return render_template('new.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['noteText']
+
+        from datetime import date
+        today = date.today()
+
+        today = today.strftime("%m-%d-%Y")
+        new_record = Question(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
+        return redirect(url_for('get_notes'))
+    else:
+        return render_template('new.html', user = a_user)
 
 
 
